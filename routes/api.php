7a3ea85665a837c6487bf4f5d0a88aecffd5446f
andres felipe de la cruz <86587545+xANDRESDELACRUZX@users.noreply.github.com>
2024-08-16 05:45:02 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,19 @@ Route::post('/user/status/{id}', [UserController::class, 'statedUser'] );
 //user como archivos fuera del controler como base logic
 Route::match(['get', 'post'], 'userRequest', [UserController::class, 'userRequest']);
 
+//auth
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login'] );
+    Route::post('logout', [AuthController::class, 'logout'] );
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
 
 
